98e612e718e7734d2104cc790d665e054c6d325d
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-10-25 15:11:43 -0400

    Added basic RESTful express service with User model

---
+++ routes/users.js
@@ -0,0 +1,125 @@
+// require express and schema dependencies
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// Get one user
+router.get('/:id', getUser, async (req, res) => {
+    res.json(res.user)
+})
+
+// Create one user
+router.post('/', async (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        friends: [],
+        pending_friends_sent: [],
+        pending_friends_received: [],
+        pending_game_invites: [],
+        active_games: [],
+        games: []
+      })
+    
+      try {
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+      } catch (err) {
+        res.status(400).json({ message: err.message })
+      }
+})
+
+// Update one user
+router.put('/:id', getUser, async (req, res) => {
+    if (req.body.username != null) { // check username
+        res.user.username = req.body.username
+    }
+    if (req.body.password != null) { // check password
+        res.user.password = req.body.password
+    }
+    // if (!arraysEqual(req.body.friends, req.user.friends)){ // check friends
+    //     res.user.friends = req.body.friends
+    // }
+    // if (!arraysEqual(req.body.pending_friends_sent, req.user.pending_friends_sent)){ // check pending_friends_sent
+    //     res.user.pending_friends_sent = req.body.pending_friends_sent
+    // }
+    // if (!arraysEqual(req.body.pending_friends_received, req.user.pending_friends_received)){ // check pending_friends_received
+    //     res.user.pending_friends_received = req.body.pending_friends_received
+    // }
+    // if (!arraysEqual(req.body.pending_game_invites, req.user.pending_game_invites)){ // check pending_game_invites
+    //     res.user.pending_game_invites = req.body.pending_game_invites
+    // }
+    // if (!arraysEqual(req.body.active_games, req.user.active_games)){ // check active_games
+    //     res.user.active_games = req.body.active_games
+    // }
+    // if (!arraysEqual(req.body.games, req.user.games)){ // check games
+    //     res.user.games = req.body.games
+    // }
+
+    try {
+        const updatedUser = await res.user.save()
+        res.json(updatedUser)
+    } catch {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+// Delete one user
+router.delete('/:id', getUser, async (req, res) => {
+    try {
+        await res.user.remove()
+        res.json({ message: 'Deleted This User' })
+    } catch(err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// Reusable function thats gets a single user, helpful for GET by id, UPDATE, DELETE
+async function getUser(req, res, next) {
+    try {
+      user = await User.findById(req.params.id)
+      if (user == null) {
+        return res.status(404).json({ message: 'Cant find user'})
+      }
+    } catch(err){
+      return res.status(500).json({ message: err.message })
+    }
+  
+    res.user = user
+    next() // move onto the next section of code (the rest of the specific route method logic)
+  }
+module.exports = router
+
+// helper method to check if arrays match
+function arraysEqual(a, b) {
+    if(!a && !b){
+        console.log("both undefined")
+    } else if(a && !b){
+        console.log("current undefined")
+    } else if(!a && b){
+        console.log("new undefined")
+    }
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+  
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+    // Please note that calling sort on an array will modify that array.
+    // you might want to clone your array first.
+  
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+// reference environment variables using dotenv package
+require('dotenv').config()
+
+// install server dependencies
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+// connect to mongodb database
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
+// tell express to accept JSON as the data format
+app.use(express.json())
+
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
+
+// test out server is working
+app.listen(3000, () => console.log('server started'))
